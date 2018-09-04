1bb2c6c1f2d5b441e5e6e8a84f9c74a6f376e11d
Tarun Jhade <tarun@i-techsoftware.com>
2018-09-04 16:48:08 -0400

    Initial setup for redux with actions, reducers and store

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            course: { title: "" }
+        };
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({ course: course });
+    }
+
+    onClickSave() {
+        alert(this.state.course.title);
+    }
+
     render() {
         return (
             <div className="jumbotron">
                 <h1>Courses</h1>
-                <p>React, Redux and React Router in ES6</p>
+                <h2>Add Course</h2>
+                <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
+                <input type="submit" onClick={this.onClickSave} value="Save" />
             </div>
         );
     }
