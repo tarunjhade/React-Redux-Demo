68be117ec7fa54fb44758b8a14a1b5bab2922e85
Tarun Jhade <tarun@i-techsoftware.com>
2018-09-04 17:46:28 -0400

    Connecting container component to redux store

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,12 +1,14 @@
-import React from 'react';
-
+import React, { PropTypes } from 'react';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as courseActions from "../../actions/courseActions";
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
             course: { title: "" }
         };
-
+        // bind the functions
         this.onTitleChange = this.onTitleChange.bind(this);
         this.onClickSave = this.onClickSave.bind(this);
     }
@@ -18,13 +20,18 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave() {
-        alert(this.state.course.title);
+        this.props.actions.createCourse(this.state.course);
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
     }
 
     render() {
         return (
             <div className="jumbotron">
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                 <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
                 <input type="submit" onClick={this.onClickSave} value="Save" />
@@ -32,5 +39,25 @@ class CoursesPage extends React.Component {
         );
     }
 }
+// add propTypes validations
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+    return {
+        courses: state.courses
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+//const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+//export default connectedStateAndProps(CoursesPage);
+// or use below shorthand for connecting to redux
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
