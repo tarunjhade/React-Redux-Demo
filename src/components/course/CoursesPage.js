90d45b8fc2688d36fb0a375c1d08bc9171959423
Tarun Jhade <tarun@i-techsoftware.com>
2018-09-05 12:32:08 -0400

    Adding thunk for async data load

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,25 +2,13 @@ import React, { PropTypes } from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as courseActions from "../../actions/courseActions";
+import CourseList from "./CourseList";
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
             course: { title: "" }
         };
-        // bind the functions
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
@@ -28,13 +16,11 @@ class CoursesPage extends React.Component {
     }
 
     render() {
+        const { courses } = this.props;
         return (
             <div className="jumbotron">
                 <h1>Courses</h1>
-                {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
-                <input type="submit" onClick={this.onClickSave} value="Save" />
+                <CourseList courses={courses} />
             </div>
         );
     }
