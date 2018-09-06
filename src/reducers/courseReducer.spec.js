d7e1c0d7ee3a4825ea04dd39c22bb5f15343091e
Tarun Jhade <tarun@i-techsoftware.com>
2018-09-06 12:43:23 -0400

    Adding integration test for store

--- src/reducers/courseReducer.spec.js
+++ src/reducers/courseReducer.spec.js
@@ -9,14 +9,10 @@ describe('Course Reducer', () => {
             { title: 'A' },
             { title: 'B' }
         ];
-
         const newCourse = { title: 'C' };
-
         const action = actions.createCourseSuccess(newCourse);
-
         // act
         const newState = courseReducer(initialState, action);
-
         // assert
         expect(newState.length).toEqual(3);
         expect(newState[0].title).toEqual('A');
@@ -31,15 +27,12 @@ describe('Course Reducer', () => {
             { id: 'B', title: 'B' },
             { id: 'C', title: 'C' }
         ];
-
         const course = { id: 'B', title: 'New Title' };
         const action = actions.updateCourseSuccess(course);
-
         // act
         const newState = courseReducer(initialState, action);
         const updatedCourse = newState.find(a => a.id == course.id);
         const untouchedCourse = newState.find(a => a.id == 'A');
-
         // assert
         expect(updatedCourse.title).toEqual('New Title');
         expect(untouchedCourse.title).toEqual('A');
